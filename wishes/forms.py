1fc5603e244049526abc6cd8cf47910e64d0baf1
Brandon  Michel Caldarella <99013590+bmcaldarella@users.noreply.github.com>
2023-07-23 17:16:51 +0100

    Delete wishes directory

--- wishes/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Create_vote
-
-class EditVoteForm(forms.ModelForm):
-    class Meta:
-        model = Create_vote
-        fields = ['nameVote', 'description', 'imageVote']
