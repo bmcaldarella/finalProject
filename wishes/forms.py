9d50ab63eb605f2c6d07f06341f5f896c862945d
Brandon  Michel Caldarella <99013590+bmcaldarella@users.noreply.github.com>
2023-08-15 18:58:22 +0100

    Add files via upload

---
+++ wishes/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Create_vote
+
+
+
+class EditVoteForm(forms.ModelForm):
+    class Meta:
+        model = Create_vote
+        fields = ['nameVote', 'description', 'imageVote']
